2e8a8a79dd73ffa038aeb14aefe925978b5b6808
minavukovic <mivukovi@ucsd.edu>
2020-02-06 09:57:30 -0800

    done

--- routes/add.js
+++ routes/add.js
@@ -3,12 +3,13 @@ var data = require("../data.json");
 exports.addFriend = function(request, response) {    
 	var name = request.query.name;
 	var description = request.query.description;
+	var imageURL = "http://lorempixel.com/400/400/people";
 
 	var newFriend = 
-			{ 	"name": name, 
-				"description": description, 
-				"imageURL": 'http://lorempixel.com/400/400/people'
-			};
+		{ 	"name": name, 
+			"description": description, 
+			"imageURL": imageURL
+		};
 	console.log(newFriend);
 	data.friends.push(newFriend);
 	response.render('index', data);
